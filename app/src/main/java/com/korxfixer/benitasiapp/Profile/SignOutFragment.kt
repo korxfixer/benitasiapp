6a929eb61a505f76c323c2d9ba066259f9b98d7d
korxfixer <korxfixer@outlook.com>
2020-01-01 15:57:37 +0300

    Git 1 inci

---
+++ app/src/main/java/com/korxfixer/benitasiapp/Profile/SignOutFragment.kt
@@ -0,0 +1,45 @@
+package com.korxfixer.benitasiapp.Profile
+
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AlertDialog
+
+
+import com.google.firebase.auth.FirebaseAuth
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SignOutFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        var alert = AlertDialog.Builder(this!!.activity!!)
+                .setTitle("Instagram'dan Çıkış Yap")
+                .setMessage("Emin misiniz ?")
+                .setPositiveButton("Çıkış Yap", object : DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface?, which: Int) {
+
+                        FirebaseAuth.getInstance().signOut()
+                        activity!!.finish()
+                    }
+
+                })
+                .setNegativeButton("İptal", object : DialogInterface.OnClickListener {
+                    override fun onClick(dialog: DialogInterface?, which: Int) {
+                        dismiss()
+                    }
+                }).create()
+
+        return alert
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/korxfixer/benitasiapp/Share/CompressandLoadingFragment.kt
@@ -0,0 +1,34 @@
+package com.korxfixer.benitasiapp.Share
+
+
+import android.graphics.PorterDuff
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.core.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.korxfixer.benitasiapp.R
+
+import kotlinx.android.synthetic.main.fragment_yukleniyor.view.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class CompressandLoadingFragment : DialogFragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        var view=inflater.inflate(R.layout.fragment_compress_and_loading, container, false)
+
+        view.progressBar2.indeterminateDrawable.setColorFilter(ContextCompat.getColor(activity!!,R.color.siyah), PorterDuff.Mode.SRC_IN)
+
+        return view
+    }
+
+}// Required empty public constructor
